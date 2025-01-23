fbb658389aa6f47c6402b0a345f4df88dd7cd56e
Sohan5002 <sohansahu2025@gmail.com>
2025-01-23 18:38:56 +0530

    addfiles

--- src/SortingAlgo/margeesort/Arrange.java
+++ src/SortingAlgo/margeesort/Arrange.java
@@ -1,53 +1,70 @@
-package SortingAlgo.margeesort;
+package SearchingSorting.margeesort;
 
 import java.util.Scanner;
 
-
 public class Arrange {
-    public static void display()
-    public static void Merge(int arr[],int l,int mid,int r){
-        int n1=mid-l+1;
-        int n2=r-mid;
-        int L[]=new int[n1];
-        int R[]=new int[n2];
-        for(int i=0;i<n1;i++){
-          L[i]=arr[l+1];
-        }
-        for(int j=0;j<n2;j++)
-        {
-            R[j]=arr[mid+1+j];
-        }
-        int i=0;
-        int j=0;
-        int k=l;
-        while(i<n1&&j<n2)
-        {
-            if(L[i]<R[j])
-            arr[k++]=L[i++];
-        }
-        while(j<n2)
 
-        {
-            arr[k++]=R[j++];
+    public static void display(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void Merge(int arr[], int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        // Copy remaining elements of L[], if any
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+
+        // Copy remaining elements of R[], if any
+        while (j < n2) {
+            arr[k++] = R[j++];
         }
     }
-    public static void Search(int []arr,int l,int r){
-        if(l<r){
-            int mid=(l+r)/2;
-            Search(arr,l,mid);
-            Search(arr,mid+1,r);
-            Merge(arr,l,mid,r);
+
+    public static void Search(int[] arr, int l, int r) {
+        if (l < r) {
+            int mid = (l + r) / 2;
+            Search(arr, l, mid);
+            Search(arr, mid + 1, r);
+            Merge(arr, l, mid, r);
         }
     }
-    public static void  main (String[]args)
-{
-    Scanner sc = new Scanner(System.in);
-    int n= sc.nextInt();
-    int arr[]=new int [n];  
-    for(int i=0;i<n;i++){
-        arr[i]=sc.nextInt();
-    }   
-    Search(arr,0,n-1);
-    
-}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Search(arr, 0, n - 1);
+        display(arr);
+        sc.close();
+    }
 }
